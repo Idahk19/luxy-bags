4ef289bf2dceb7452e783308e88f36324ab533a3
Idahk19 <idahkarwitha9@gmail.com>
2026-06-27 20:39:37 +0300

    Create a product card function

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,28 @@
+function ProductCard ({product}){
+    return(
+         <div className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl transition">
+      <img
+        src={product.image}
+        alt={product.name}
+        className="w-full h-72 object-cover"
+      />
+
+      <div className="p-4">
+        <h2 className="text-xl font-semibold">{product.name}</h2>
+
+        <p className="text-gray-600 mt-2">
+          {product.description}
+        </p>
+
+        <p className="text-pink-600 font-bold text-lg mt-3">
+          {product.price}
+        </p>
+
+        <button className="mt-4 w-full bg-black text-white py-2 rounded hover:bg-gray-800">
+          Add to Cart
+        </button>
+      </div>
+    </div>
+
+    )
+}
