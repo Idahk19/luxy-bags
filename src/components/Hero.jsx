72e832296d7c0bf2c654dd22766fde37ee409241
Idahk19 <idahkarwitha9@gmail.com>
2026-07-03 11:09:21 +0300

    Change background color on the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 function Hero(){
     return(
-       <section className="bg-pink-100 text-center flex items-center flex-col py-32 justify-center">
+       <section className="bg-pink-200 text-center flex items-center flex-col py-32 justify-center">
         <h1 className="text-5xl font-bold text-pink-700 mb-4">Welcome to Luxy Bags</h1>
         <p className="text-gray-700 text-lg max-w-2xl mb-6">Discover stylish handbags designed to make every outfit stand out.</p>
         <button className="bg-pink-600 text-white px-6 py-3 rounded-lg hover:bg-pink-700">Shop Now</button>
