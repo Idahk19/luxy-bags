35c2e0e78daa04d1b47445473a135e741fc828fd
Idahk19 <idahkarwitha9@gmail.com>
2026-07-04 13:07:34 +0300

    Add hover on the navigation links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,23 +2,25 @@ import { Link } from 'react-router-dom'
 
 function Navbar(){
     return(
-   <nav className="sticky top-0 z-50 w-full border-b bg-pink-300/60 bg-background/95  backdrop-blur supports-[backdrop-filter]:bg-background/60 flex justify-between items-center p-4 ">
+   <nav className="sticky top-0 z-50 w-full border-b bg-pink-300/60
+    bg-background/95  backdrop-blur supports-[backdrop-filter]:bg-background/60
+     flex justify-between items-center p-4 ">
       <h1 className="text-2xl font-bold">Luxy Bags</h1>
 
-      <ul className="flex gap-6">
-        <li>
+      <ul className="flex gap-6 ">
+        <li className="hover:text-pink-500">
           <Link to="/"> Home</Link>
         </li>
 
-        <li>
+        <li className="hover:text-pink-500">
           <Link to="/products"> Products</Link>
         </li>
 
-        <li>
+        <li className="hover:text-pink-500">
           <Link to="/about"> About Us</Link>
         </li>
 
-        <li>
+        <li className="hover:text-pink-500">
           <Link to="/contact"> Contact Us</Link>
         </li>
       </ul>
