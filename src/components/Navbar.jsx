1ec840c255b1e287c1df52172cb5b84c3ebba6b1
Idahk19 <idahkarwitha9@gmail.com>
2026-07-04 18:18:39 +0300

    Add a menu bar for mobile screens responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,103 @@
-import { Link } from 'react-router-dom'
-import { ShoppingCart } from "lucide-react";
-import { useContext } from 'react';
-import { CartContext } from '../context/CartContext';
+import { Link } from "react-router-dom";
+import {
+  ShoppingCart,
+  Menu,
+  House,
+  ShoppingBag,
+  Info,
+  Phone,
+  User,
+} from "lucide-react";
+import { useContext } from "react";
+import { useState } from "react";
+import { CartContext } from "../context/CartContext";
 
-function Navbar(){
 
+function Navbar() {
   const { cart, setCart } = useContext(CartContext);
-    return(
-   <nav className="sticky top-0 z-50 w-full border-b bg-pink-300/60
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav
+      className="sticky top-0 z-50 w-full border-b bg-pink-300/60
     bg-background/95  backdrop-blur supports-[backdrop-filter]:bg-background/60
-     flex justify-between items-center p-4 ">
-      <h1 className="text-2xl font-bold">Luxy Bags</h1>
+     flex  justify-between items-center p-4 "
+    >
+    <h1 className="text-2xl font-bold">Luxy Bags</h1>
+     <button className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
+        <Menu size={28} />
+      </button>
+        
+        <div className="hidden md:flex items-center justify-between flex-1">
+          <ul className="flex gap-6 mx-auto">
+            <li className="hover:text-pink-500 flex items-center gap-2">
+              <House className="w-4 h-4" />
+              <Link to="/"> Home</Link>
+            </li>
 
-      <ul className="flex gap-6 ">
-        <li className="hover:text-pink-500">
-          <Link to="/"> Home</Link>
-        </li>
+            <li className="hover:text-pink-500 flex items-center gap-2">
+              <ShoppingBag className="w-4 h-4" />
+              <Link to="/products"> Products</Link>
+            </li>
 
-        <li className="hover:text-pink-500">
-          <Link to="/products"> Products</Link>
-        </li>
+            <li className="hover:text-pink-500 flex items-center gap-2">
+              <Info className="w-4 h-4" />
+              <Link to="/about"> About Us</Link>
+            </li>
 
-        <li className="hover:text-pink-500">
-          <Link to="/about"> About Us</Link>
-        </li>
+            <li className="hover:text-pink-500 flex items-center gap-2">
+              <Phone className="w-4 h-4" />
+              <Link to="/contact"> Contact Us</Link>
+            </li>
+          </ul>
+          <Link to="/cart" className="relative">
+            <ShoppingCart className="w-7 h-7" />
 
-        <li className="hover:text-pink-500">
-          <Link to="/contact"> Contact Us</Link>
-        </li>
-      </ul>
-      <Link to="/cart" className="relative">
-  <ShoppingCart className="w-7 h-7" />
+            {cart.length > 0 && (
+              <span className="absolute -top-2 -right-2 bg-pink-600 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
+                {cart.length}
+              </span>
+            )}
+          </Link>
+        </div>
+      
+      {isOpen && (
+       <div className="absolute top-full left-0 w-full bg-pink-100 shadow-lg md:hidden">
+       <ul className="flex flex-col p-4 gap-4">
+            <li className="hover:text-pink-500 flex items-center gap-2">
+              <House className="w-4 h-4" />
+              <Link to="/"> Home</Link>
+            </li>
 
-  {cart.length > 0 && (
-    <span className="absolute -top-2 -right-2 bg-pink-600 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
-      {cart.length}
-    </span>
-  )}
-</Link>
-    </nav> 
-    )
+            <li className="hover:text-pink-500 flex items-center gap-2">
+              <ShoppingBag className="w-4 h-4" />
+              <Link to="/products"> Products</Link>
+            </li>
+
+            <li className="hover:text-pink-500 flex items-center gap-2">
+              <Info className="w-4 h-4" />
+              <Link to="/about"> About Us</Link>
+            </li>
+
+            <li className="hover:text-pink-500 flex items-center gap-2">
+              <Phone className="w-4 h-4" />
+              <Link to="/contact"> Contact Us</Link>
+            </li>
+          </ul>
+          <Link to="/cart" className="relative">
+            <ShoppingCart className="w-7 h-7" />
+
+            {cart.length > 0 && (
+              <span className="absolute -top-2 -right-2 bg-pink-600 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
+                {cart.length}
+              </span>
+            )}
+          </Link>
+        </div>
+      )}
+    </nav>
+  );
 }
- 
+
 export default Navbar;
