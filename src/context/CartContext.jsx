7c1160c7ea73bbd2e77c6f531320aa8c95616bfc
Idahk19 <idahkarwitha9@gmail.com>
2026-07-07 15:42:05 +0300

    Update the CartContext so that  it can save so that on load it does not clear the cart

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,9 +1,21 @@
 import { createContext, useState } from "react";
+import { useEffect } from "react"
 
 export const CartContext = createContext();
 
 export function CartProvider({children}) {
-    const [cart, setCart] = useState([]);
+    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+    const cartKey = currentUser ? `cart_${currentUser.email}` : "guest_cart";
+      
+    const [cart, setCart] = useState(() => {
+        const savedCart = localStorage.getItem(cartKey);
+        return savedCart ? JSON.parse(savedCart) : [];
+    });
+     useEffect(() => {
+    localStorage.setItem(cartKey, JSON.stringify(cart));
+  }, [cart, cartKey]);
+
     return(
         <CartContext.Provider value={{cart, setCart}}>
             {children}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -33,6 +33,7 @@ function Login() {
     localStorage.setItem("currentUser", JSON.stringify(user));
 
     navigate("/");
+    window.location.reload();
   };
   return (
     <div>
